2e417fcece1af4b52ca978f3386d3cb2e2d43942
Mirza Yassin <mirza.yassin9@gmail.com>
2025-11-02 19:40:59 +0400

    added css properties for degree and experiance

--- fontend/src/Pages/Appointments.jsx
+++ fontend/src/Pages/Appointments.jsx
@@ -31,16 +31,19 @@ const Appointments = () => {
         </div>
           {/* doctor info : name, degree and experiance */}
         <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
-          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>{docInfo.name} <img className='w-5' src={assets.verified_icon} alt="" /> </p>
-          <div>
-            <p>{docInfo.degree} - {docInfo.speciality}</p>
-            <button>{docInfo.experience}</button>
+          <p className='flex items-center gap-2 text-2xl font-medium text-blue-500'>{docInfo.name} <img className='w-5' src={assets.verified_icon} alt="" /> </p>
+          <div className='flex items-center gap-2 text-sm mt-1 text-[#18528f]'>
+            <p>{docInfo.degree} - <span className='font-medium'>{docInfo.speciality}</span></p>
+            <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
           </div>
           {/* doctor about */}
           <div>
-            <p>About <img src={assets.info_icon} alt="" /></p>
-            <p>{docInfo.about}</p>
+            <p className='flex items-center gap-1 text-sm font-medium mt-3 text-blue-500'>Biography</p>
+            <p className='text-sm text-[#194f90] max-w-[700] mt-1'>{docInfo.about}</p>
           </div>
+          <p className='text-blue-800'>
+            Appointment fee: <span>{docInfo.fees}</span>
+          </p>
         </div>
       </div>
     </div>
