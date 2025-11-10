e71e43b67c66ac8e5a2b1a8e7a90db5ca22b7eae
Mirza Yassin <mirza.yassin9@gmail.com>
2025-11-10 22:53:02 +0400

    feat(auth): add user registration controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,49 @@
+const validator = require('validator')
+const bycrypt = require('bcrypt')
+const userModel = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+// API to register user
+const registerUser = async(req,res)=>{
+    try {
+        const {name,email,password} = req.body
+
+        if(!name || !email || !password){
+            return res.json({success:false,message:"Missing Details"})
+        }
+
+        // validating email format
+
+        if(!validator.isEmail(email)){
+           return res.json({success:false,message:"enter a valid email"})
+        }
+
+        // validating strong password
+
+           if(password.length < 8){
+           return res.json({success:false,message:"enter a strong password"})
+        }
+
+        // hashing user password
+        const salt = await bycrypt.genSalt(10)
+        const hashedPassword = await bycrypt.hash(password,salt)
+
+        const userData = {
+            name,
+            email,
+            password:hashedPassword
+        }
+
+        const newUser = new userModel(userData)
+        const user = await newUser.save()
+        
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+        res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+module.exports = {registerUser}
