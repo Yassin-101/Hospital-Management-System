3621e94aa71401c6fcc3921bc831a398ed482336
Mirza Yassin <mirza.yassin9@gmail.com>
2025-11-10 20:55:04 +0400

    feat(backend): implement changeAvailability

---
+++ backend/controllers/doctorController.js
@@ -0,0 +1,22 @@
+const doctorModel = require("../models/doctor")
+
+const changeAvailability = async(req,res)=>{
+
+
+    try{
+
+        const {docId} = req.body
+        const docData = await doctorModel.findById(docId)
+        await doctorModel.findByIdAndUpdate(docId,{available:!docData.available})
+        res.json({success:true,message:'Avalability Changed'})
+
+    }catch (error){
+          console.log(error)
+        res.json({success:false,message:error.message})
+    }
+
+
+
+}
+
+module.exports = {changeAvailability}
