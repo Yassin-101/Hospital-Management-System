f6eca9942252ac405c46bcd73d7960d1d6de6950
Mirza Yassin <mirza.yassin9@gmail.com>
2025-11-12 22:14:30 +0400

    feat(user): add update profile API and routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const {registerUser, loginUser, getProfile} = require('../controllers/userController')
+const {registerUser, loginUser, getProfile, updateProfile} = require('../controllers/userController')
 const authUser = require('../middlewares/authUser')
+const upload = require('../middlewares/multer')
 
 const userRouter = express.Router()
 
@@ -8,5 +9,6 @@ userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 
 userRouter.get('/get-profile',authUser,getProfile)
+userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
 
 module.exports = userRouter
