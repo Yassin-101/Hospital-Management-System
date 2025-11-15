e77d85a9f77c2b5ed80fb0b8bca5989ef989e1a9
Mirza Yassin <mirza.yassin9@gmail.com>
2025-11-15 21:29:17 +0400

    feat(user): add function to fetch user profile data

--- fontend/src/Context/AppContext.jsx
+++ fontend/src/Context/AppContext.jsx
@@ -11,6 +11,8 @@ const AppContextProvider = (props)=>{
     const [doctors,setDoctor] = useState([])
 
     const [token,setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
+    // store user data
+    const [userData,setUserData] = useState(false)
   
 
     const getDoctorsData = async()=>{
@@ -27,13 +29,35 @@ const AppContextProvider = (props)=>{
         }
     }
 
+    const loadUserProfileData = async()=>{
+        try {
+            const {data} = await axios.get(backendUrl + '/api/user/get-profile',{headers:{token}})
+            if (data.success) {
+                setUserData(data.userData)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
       const value = {
-        doctors,currencySymbol,token,setToken,backendUrl
+        doctors,currencySymbol,token,setToken,backendUrl,setUserData,userData,loadUserProfileData
     }
 
     useEffect(()=>{
         getDoctorsData()
     },[])
+
+    useEffect(()=>{
+       if (token) {
+        loadUserProfileData()
+       }else{
+        setUserData(false)
+       }
+    },[token])
 
     return (
         <AppContext.Provider value={value}>
