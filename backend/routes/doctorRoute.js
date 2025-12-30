6d5c3b89277a490c5b89eb4e5d686932cc65d835
Mirza Yassin <mirza.yassin9@gmail.com>
2025-12-30 13:55:29 +0400

    feat: add doctor login API

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { doctorList } = require('../controllers/doctorController');
+const { doctorList, loginDoctor } = require('../controllers/doctorController');
 
 const doctorRouter = express.Router();
 
 doctorRouter.get('/list',doctorList)
-
+doctorRouter.post('/login',loginDoctor)
 module.exports = doctorRouter
