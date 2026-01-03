7e564c092f53db76ceecef0d394ad5e663986864
Mirza Yassin <mirza.yassin9@gmail.com>
2026-01-03 20:59:40 +0400

    feat: add appointments doctor controller and routes

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { doctorList, loginDoctor } = require('../controllers/doctorController');
+const { doctorList, loginDoctor, appointmentsDoctor } = require('../controllers/doctorController');
+const authDoctor = require('../middlewares/authDoctor');
 
 const doctorRouter = express.Router();
 
 doctorRouter.get('/list',doctorList)
 doctorRouter.post('/login',loginDoctor)
+doctorRouter.get('/appointments',authDoctor, appointmentsDoctor)
 module.exports = doctorRouter
