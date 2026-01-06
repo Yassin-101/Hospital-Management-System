5eec2f95b56305cde9a4f22cd3d343ff0a89e340
Mirza Yassin <mirza.yassin9@gmail.com>
2026-01-06 13:20:26 +0400

    feat: add doctor dashboard API

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel } = require('../controllers/doctorController');
+const { doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel , doctorDashboard} = require('../controllers/doctorController');
 const authDoctor = require('../middlewares/authDoctor');
 
 const doctorRouter = express.Router();
@@ -9,4 +9,5 @@ doctorRouter.post('/login',loginDoctor)
 doctorRouter.get('/appointments',authDoctor, appointmentsDoctor)
 doctorRouter.post('/complete-appointment', authDoctor, appointmentComplete)
 doctorRouter.post('/cancel-appointment', authDoctor, appointmentCancel)
+doctorRouter.get('/dashboard', authDoctor, doctorDashboard)
 module.exports = doctorRouter
